547c1d14247530f7f47b1bd5c8e0bbe5b77a2936
Colin But <colinbut@users.noreply.github.com>
2017-11-14 22:12:10 +0000

    Project initial source files

---
+++ src/main/java/com/mycompany/processcalculator/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.processcalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
